6372fe5a7156b29b10d919454fd391d170a5f4e9
Luke Thomas <luk3thomas@gmail.com>
2014-01-08 23:52:54 -0600

    cleanup

--- config.rb
+++ config.rb
@@ -6,67 +6,9 @@ require './lib/helpers.rb'
 require './lib/gzip_sitemap.rb'
 
 activate :gzip_sitemap
-#activate :livereload
-
-# Susy grids in Compass
-# First: gem install susy
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
-activate :s3_sync do |sync|
-  sync.bucket = ENV['AWS_BUCKET']
-  sync.region = 'us-east-1'
-  sync.aws_access_key_id = ENV['AWS_KEY']
-  sync.aws_secret_access_key = ENV['AWS_SECRET']
-  sync.delete = true
-end
-
-caching_policy 'text/html', max_age: 0, must_revalidate: true
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 set :markdown_engine, :redcarpet
@@ -75,27 +17,8 @@ set :markdown, :fenced_code_blocks => true, :smartypants => true
 set :haml, { ugly: true }
 activate :syntax
 
-# Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
   activate :syntax
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
   activate :asset_hash
-
-  # Use relative URLs
-  activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
 end
